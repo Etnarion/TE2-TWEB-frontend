c7ba877eeb7cd2ff3f4da69f047bbf125fd562ef
Etnarion <samuelmayorweb@gmail.com>
2019-01-08 18:27:53 +0100

    front

---
+++ te2-tweb/src/components/pages/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+class SignUpPage extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default withRouter(SignUpPage)
